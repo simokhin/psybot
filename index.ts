fd92eafc7dd7cded1babeaf66a60a4d8a7f91e82
Nikita Simokhin <simokhinn@proton.me>
2025-08-27 21:54:03 +0300

    Add grammy

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Bot } from "grammy";
+
+const BOT_API = process.env.BOT_API || "";
+
+const bot = new Bot(BOT_API);
+
+bot.command("start", (ctx) => ctx.reply("Hello! I am your bot."));
+
+bot.on("message", (ctx) => ctx.reply("You said: " + ctx.message.text));
+
+bot.start();
